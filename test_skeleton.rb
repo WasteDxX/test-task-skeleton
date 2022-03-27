f71951777e47fa2294af33a029e8ee90e4aa59ac
WasteDxX <34161005+WasteDxX@users.noreply.github.com>
2022-03-27 19:38:51 +0300

    Add files via upload

--- test_skeleton.rb
+++ test_skeleton.rb
@@ -10,7 +10,7 @@ class TestSkeleton
   # TestSkeleton.new.even_or_odd(0) should return "even"
   # TestSkeleton.new.even_or_odd(-42) should return "even"
   def even_or_odd(number)
-    # Your solution should be here
+    number.remainder(2) == 0 ? "even" : "odd"
   end
 
   # https://www.codewars.com/kata/5583090cbe83f4fd8c000051
@@ -21,7 +21,7 @@ class TestSkeleton
   # TestSkeleton.new.reverse_array(348597) should return [7,9,5,8,4,3]
   # TestSkeleton.new.reverse_array(0) should return [0]
   def reverse_array(number)
-    # Your solution should be here
+    number.to_s.split('').map(&:to_i).reverse
   end
 
   # https://www.codewars.com/kata/554b4ac871d6813a03000035
@@ -36,7 +36,8 @@ class TestSkeleton
   # There will always be at least one number in the input string.
   # Output string must be two numbers separated by a single space, and highest number is first.
   def high_and_low(test_string)
-    # Your solution should be here
+    test_string = test_string.to_s.split(' ').map(&:to_i).sort.reverse
+    test_string[0].to_s+" "+test_string[-1].to_s
   end
 
   # https://www.codewars.com/kata/5b16490986b6d336c900007d
@@ -49,7 +50,14 @@ class TestSkeleton
   # TestSkeleton.new.my_languages({"Hindi" => 60, "Dutch" => 93, "Greek" => 71}) should return ["Dutch", "Greek", "Hindi"]
   # TestSkeleton.new.my_languages({"C++" => 50, "ASM" => 10, "Haskell" => 20}) should return [] 
   def my_languages(hash)
-    # Your solution should be here
+    result = []
+    hash = hash.sort_by {|_key, value| value}
+    hash.each do |key, value|
+      if value >= 60
+        result.push(key)
+      end
+    end
+    return result.reverse
   end
 
   # https://www.codewars.com/kata/563089b9b7be03472d00002b
@@ -62,7 +70,14 @@ class TestSkeleton
   # values_list = [1, 3, 4, 2]
   # TestSkeleton.new.remove_array_elements(integer_list, values_list) should return [5, 6 ,7 ,8]
   def remove_array_elements(source_array, values_array)
-    # Your solution should be here
+    values_array.each do |i|
+      source_array.each do |j|
+        if i == j
+          source_array.delete(j)
+        end
+      end    
+    end
+    return source_array
   end
 
   # https://www.codewars.com/kata/5b39e91ee7a2c103300018b3
@@ -72,7 +87,18 @@ class TestSkeleton
   # string = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
   # TestSkeleton.new.consecutive_duplicates(string) should return "alpha beta gamma delta alpha beta gamma delta"
   def consecutive_duplicates(string)
-    # Your solution should be here
+    # your code here
+    result_str = []
+    i = 0
+    new_arr = string.split(" ")
+    while i < new_arr.length
+      if  new_arr[i] != new_arr[i+1]
+        result_str= result_str.push(new_arr[i])
+      end
+      i+=1
+    end
+    result_str =result_str.map { |i| i.to_s }.join(" ")
+    return result_str
   end
 
   # https://www.codewars.com/kata/56747fd5cb988479af000028
@@ -90,6 +116,7 @@ class TestSkeleton
   # Output:
   # The middle character(s) of the word represented as a string.
   def middle_chars(test_string)
-    # Your solution should be here
+    str_len = test_string.length
+    str_len % 2 != 0 ? test_string[(str_len /2).round(half: :up)] : test_string[str_len/2 - 1] + test_string[str_len/2]
   end
 end
